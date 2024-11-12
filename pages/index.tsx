90df7d7f4a49bd924ec38947ad4253bc58c365ac
surya wiliam <eswuis@gmail.com>
2024-11-12 20:43:12 +0700

    asdasd

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import SessionForm from "@/components/SessionForm";
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold mb-6">Welcome to Live Chat</h1>
+      <h1 className="text-3xl font-bold mb-6">Welcome to Live Chata</h1>
       <SessionForm />
     </div>
   );
